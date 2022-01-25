8e1292937daac987da6f8e54c4ccb7663368cb2c
sitdisch <sitdisch@gmx.de>
2022-01-25 11:10:31 +0100

    deps: updated | canvases: spaces added

--- src/canvas/mtw-canvas-malachite/main.js
+++ src/canvas/mtw-canvas-malachite/main.js
@@ -24,29 +24,38 @@ window.addEventListener("load",
     const gl = document.getElementById("header-canvas").getContext("webgl");
     resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    
     // FIX: adding "\n" is a workaround for twgl.js error:'no element with id:...'
     const vs = "\n"+GLSLX_SOURCE_VERTEX_SHADER;
     const fs = "\n#ifdef GL_FRAGMENT_PRECISION_HIGH\nprecision highp float;\n#else\nprecision mediump float;\n#endif\n"+GLSLX_SOURCE_FRAGMENT_SHADER;
+    
     const programInfo = createProgramInfo(gl, [vs, fs]);
     gl.useProgram(programInfo.program);
+    
     const arrays = {
       [GLSLX_NAME_POSITION]: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
     };
+    
     const bufferInfo = createBufferInfoFromArrays(gl, arrays);
     setBuffersAndAttributes(gl, programInfo, bufferInfo);
+    
     const uniforms = {
       [GLSLX_NAME_I_RESOLUTION]: [gl.canvas.width, gl.canvas.height, 1],
       [GLSLX_NAME_I_TIME]: 0,
     };
+    
     const randomStart = Math.floor(Math.random() * 100);
+    
     function render() {
       uniforms[GLSLX_NAME_I_TIME] = randomStart + performance.now() / 4500;
       setUniforms(programInfo, uniforms);
       drawBufferInfo(gl, bufferInfo);
     };
+    
     const canvasOvershadow = document.getElementById("header-canvas-overshadow");
     const headerEnd = document.getElementById("header-end").offsetTop;
     var overshadow = true;
+    
     window.setInterval(() => {
       if ( headerEnd > window.pageYOffset ) {
         requestAnimationFrame(render);
